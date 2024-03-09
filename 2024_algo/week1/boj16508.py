25cd58770a879526eff0e993a2ab3ae9d633e8ac
youngin kim <hardworkingini@gmail.com>
2024-03-09 23:47:31 +0900

    modify 16508.py

--- 2024_algo/week1/boj16508.py
+++ 2024_algo/week1/boj16508.py
@@ -1,33 +1,32 @@
 # 전공책
 import sys
+from math import *
+from itertools import *
 input = sys.stdin.readline
 T = input().strip()
 N = int(input())
 majors = [input().split() for _ in range(N)]
 majors.sort(key=lambda x : (x[0]))
-ans = 100001
+ans = inf # 최소 금액
 intersection = [[] for _ in range(N)]
+price = [int(major[0]) for major in majors]
 i = 0
+start = 0
+
+def nCr(n, r):
+    if r == 1:
+        return
+    cnt = comb(n, r)
+
+    for i in range(cnt):
+
+        return
+
 for major in majors:
-    print(major)
     for alphabet in T:
         if alphabet in major[1]:
             intersection[i].append(alphabet)
+            T.replace(alphabet,'',1)
     i += 1
 
-for i in range(N):
-    temp = 0 # 가격 임시 저장
-    find = set(list(T)) - set(intersection[i])
-    temp += int(majors[i][0])
-    while len(find) != 0:
-        for j in range(N):
-            if i == j:
-                continue
-            tmp = find - set(intersection[j])
-            if find - tmp != None:
-                temp += int(majors[j][0])
-                find = tmp
-    ans = min(ans, temp)
-
 print(ans)
-
