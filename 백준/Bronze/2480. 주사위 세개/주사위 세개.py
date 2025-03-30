b139d35ec585809935e131997749a41d48a4d2fe
Youngin Kim <hardworkingini@gmail.com>
2025-03-31 00:58:06 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+reward = 0
+
+if(a == b and b == c) :
+    reward = 10000 + a * 1000
+elif (a != b and b != c and c != a):
+    reward = max(a, b, c) * 100
+elif (a == b or a == c):
+    reward = 1000 + a * 100
+elif (b == c):
+    reward = 1000 + b * 100
+
+print(reward)
