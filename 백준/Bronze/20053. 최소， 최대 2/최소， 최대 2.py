8a355afa5cb3b19dec5871d4b4e3a0016f1faa77
Youngin Kim <hardworkingini@gmail.com>
2025-07-12 14:24:04 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 828 ms, Memory: 172088 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+ans = []
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    print(min(arr), max(arr))
