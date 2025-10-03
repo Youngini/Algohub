317359279bbc2f49c36a373ed7696e7a86fb78a2
Youngin Kim <hardworkingini@gmail.com>
2025-10-04 02:32:00 +0900

    [level 0] Title: 배열의 평균값, Time: 4.73 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,11 +1,6 @@
+import java.util.Arrays;
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        double temp = 0;
-        for (int i = 0; i < numbers.length; i++){
-            temp += numbers[i];
-        }
-        answer = temp / numbers.length;
-        return answer;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
