16fc31bba2c4de81227007aa16b6fa33dd4f9a8d
Youngin Kim <hardworkingini@gmail.com>
2025-10-04 02:05:15 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 4;
-        if (angle < 90){
-            answer = 1;
-        }
-        else if(angle == 90){
-            answer = 2;
-        }
-        else if(angle < 180){
-            answer = 3;
-        }
-        return answer;
+        return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4;
         
     }
 }
