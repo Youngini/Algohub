b08a6a1c64efd5720ddd9c2e2a85bfb27c31a9f7
Youngin Kim <hardworkingini@gmail.com>
2025-10-06 04:44:41 +0900

    [level 0] Title: A로 B 만들기, Time: 0.75 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(String before, String after) {
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b) ? 1 : 0;
+    }
+}
