b5ba730f08dc2e574cf7808799999c85279d8d76
Youngin Kim <hardworkingini@gmail.com>
2025-10-06 00:32:53 +0900

    [level 0] Title: 소인수분해, Time: 16.22 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 2 ; i <= n; i++){
+            if(n % i == 0){
+                list.add(i);
+                while(n % i == 0){
+                    n = n / i;
+                }
+            }
+        }
+        return list.stream().mapToInt(x -> x).toArray();
+    }
+}
