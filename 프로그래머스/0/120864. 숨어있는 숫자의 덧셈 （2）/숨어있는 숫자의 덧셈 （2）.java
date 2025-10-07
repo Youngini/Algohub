3bc05f7ae2dcb62bc35de58c87d452afe4bc1c03
Youngin Kim <hardworkingini@gmail.com>
2025-10-07 19:23:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.06 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        int temp = 0;
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) {
+                temp = temp * 10 + (c - '0');
+            } else {
+                answer += temp;
+                temp = 0;
+            }
+        }
+        answer += temp;
+        return answer;
+    }
+}
