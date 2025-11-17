63f8ef636a7816aa56f6af58f9c47c5ed555b079
Youngin Kim <hardworkingini@gmail.com>
2025-11-18 08:47:40 +0900

    [Gold III] Title: 당근 클릭 게임, Time: 584 ms, Memory: 113232 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31434. 당근 클릭 게임/당근 클릭 게임.py
+++ 백준/Gold/31434. 당근 클릭 게임/당근 클릭 게임.py
@@ -9,31 +9,28 @@ for _ in range(n):
     A.append(a)
     B.append(b)
 
-MAXS = 1 + 50 * k  # 속도 최대 가능치 (여유 있게 잡음)
+dp = {1 : 0} # 속도 : 당근 수
 
-# dp[t][s] = t초에 속도 s일 때 가능한 최대 당근
-dp = [[-1] * (MAXS + 1) for _ in range(k + 1)]
-dp[0][1] = 0  # 시작: t=0, 속도=1, 당근=0
+for _ in range(k):
+    next_dp = {}
 
-for t in range(k):
-    for s in range(1, MAXS + 1):
-        if dp[t][s] == -1:
-            continue
-        
-        m = dp[t][s]  # 현재 당근
+    for s, m in dp.items():
 
         # 1. 클릭
         ns = s
         nm = m + s
-        dp[t+1][ns] = max(dp[t+1][ns], nm)
 
-        # 2. 스피드 효과 구매
+        if nm > next_dp.get(ns, -1):
+            next_dp[ns] = nm
+        
+         # 2) 스피드 구매
         for i in range(n):
             if m >= A[i]:
                 ns = s + B[i]
                 nm = m - A[i]
-                if ns <= MAXS:
-                    dp[t+1][ns] = max(dp[t+1][ns], nm)
+                if nm > next_dp.get(ns, -1):
+                    next_dp[ns] = nm
+    
+    dp = next_dp
 
-answer = max(dp[k])
-print(answer)
+print(max(dp.values()))
