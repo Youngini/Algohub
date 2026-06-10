0f4b2f79ca2cbad63a25b875b9770ae8e91a2c94
Youngin Kim <hardworkingini@gmail.com>
2026-06-10 09:00:28 +0900

    [level 0] Title: 문자열 뒤집기, Time: 4.19 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        return my_string.substring(0, s) + new StringBuilder(my_string.substring(s, e + 1)).reverse().toString() + my_string.substring(e + 1);
+    }
+}
