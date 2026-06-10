bb7ea3f0926e32c4e3fa871599d9011dcff5573e
Youngin Kim <hardworkingini@gmail.com>
2026-06-10 17:20:48 +0900

    [level 0] Title: 리스트 자르기, Time: 2.31 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        if(n == 1) return Arrays.copyOfRange(num_list, 0, slicer[1] + 1);
+        else if(n == 2) return Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+        else if(n == 3) return Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
+        else{
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = slicer[0]; i < slicer[1] + 1; i += slicer[2] ) arr.add(num_list[i]);
+            return arr.stream().mapToInt(Integer :: intValue).toArray();
+        }
+    }
+}
