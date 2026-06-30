c43f922e191ef9f9dc1a60b95d9657550975bf9f
Youngin Kim <hardworkingini@gmail.com>
2026-06-30 20:37:31 +0900

    [level 3] Title: 정수 삼각형, Time: 5.73 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -4,24 +4,19 @@ class Solution {
         int[][] dp = new int[n][n];
         dp[0][0] = triangle[0][0];
         
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0) {
-                    // 왼쪽 끝: 위쪽만 가능
-                    dp[i][j] = dp[i-1][j] + triangle[i][j];
-                } else if (j == i) {
-                    // 오른쪽 끝: 왼쪽 위만 가능
-                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
-                } else {
-                    // 중간: 둘 중 큰 값
-                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
-                }
-            }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                // 가장 왼쪽인 경우
+                if(j == 0) dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                // 가장 오른쪽인 경우
+                else if(j == i) dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                // 나머지
+                else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                
+            }   
         }
-        
-        // 마지막 층에서 최댓값
         int answer = 0;
-        for (int v : dp[n - 1]) answer = Math.max(answer, v);
+        for(int i : dp[n - 1]) answer = Math.max(answer, i);
         return answer;
     }
 }
