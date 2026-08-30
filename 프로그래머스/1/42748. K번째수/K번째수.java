a38444a235d95d175b5e711824247cae742fc1b4
Youngin Kim <hardworkingini@gmail.com>
2026-08-30 23:44:11 +0900

    [level 1] Title: K번째수, Time: 6.23 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,11 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         ArrayList<Integer> answer = new ArrayList<>();
-        for (int[] c : commands) {
-            int s = c[0] - 1;
-            int e = c[1];
-            int k = c[2] - 1;
-            
-            int[] sliced = Arrays.copyOfRange(array, s, e);
-            Arrays.sort(sliced);
-            answer.add(sliced[k]);
+        for(int[] c : commands){
+            int[] tmp = Arrays.copyOfRange(array, c[0] - 1, c[1]);
+            Arrays.sort(tmp);
+            answer.add(tmp[c[2] - 1]);
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
